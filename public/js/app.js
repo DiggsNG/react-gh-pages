bff872c680baa086c0a7973625afa6f32e6104ba
diggsn <42653979+DiggsNG@users.noreply.github.com>
2019-03-05 19:34:44 -0800

    Create a React app and publish it in Github pages

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+	$.backstretch([
+          "img/bran.jpg",
+          "img/Daenerys.jpg",
+          "img/dwolf.jpg",
+          "img/icedragons.jpg",
+          "img/dragon.jpg",
+          "img/jamieandcersie.jpg",
+          "img/jonsnow.jpg",
+          "img/kaldrago.jpg",
+          "img/rhaegar.jpg",
+          "img/tyrion.jpg",
+          "img/sonsa.jpg"
+        ], {
+            duration: 10000,
+            fade: 900
+        });
+
+  console.log('here');
+});
---
+++ src/components/GameCard/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./style.css";
+
+function GameCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.name}
+          </li>
+          <li>
+            <strong>Character:</strong> {props.character}
+          </li>
+          <li>
+            <strong>Location:</strong> {props.location}
+          </li>
+        </ul>
+      </div>
+      <span onClick={() => props.removeGame(props.id)} className="remove">
+        𝘅
+      </span>
+    </div>
+  );
+}
+export default GameCard;
